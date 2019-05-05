53d739dc28429bd08ef6fbed513253eae86a6ca6
Rish-Singhal <zzxy334@gmail.com>
2019-05-05 12:13:06 +0530

    updated desc2

--- src/Pages/AboutC/AboutC.js
+++ src/Pages/AboutC/AboutC.js
@@ -12,7 +12,7 @@ export default class AboutC extends Component {
                     <Row id='r2' className='show-grid'>
                         <Col id='hackiv-desc' md={6} lg={6}>
                             <h4>EVENTO</h4>
-                            <p>Evento is a software-based end to end solution to facilitate all activities in any kind of event or happenings which might necessitate the tracking of resources involved. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                            <p>Evento is a software-based end to end solution to facilitate all activities in any kind of event or happenings which might necessitate the tracking of resources involved.
                         </p>
                         </Col>
                         <Col md={6} mdHidden xsHidden smHidden lg={6}>
